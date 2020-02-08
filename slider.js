db3f4108f62adde3f086e9f5a656f6fa1932fb89
maserati12 <triggamaserati@gmail.com>
2020-02-08 16:54:47 +0300

    show only one slide

--- slider.js
+++ slider.js
@@ -3,3 +3,15 @@ let allSlides = document.querySelectorAll('.slide');
 let prev = document.querySelector('.previous');
 let next = document.querySelector('.next');
 // Activating the first slide on load
+let slideIndex = 1;
+// This function activates the selected slide
+const activateSlide = slide => {
+	if (slide > allSlides.length) {slideIndex = 1};
+	if (slide < 1) {slideIndex = allSlides.length};
+	for (let i = 0; i < allSlides.length; i++) {
+		allSlides[i].style.display = "none";
+	}
+	allSlides[slideIndex - 1].style.display = "grid";
+}
+
+activateSlide(slideIndex)
