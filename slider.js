4ecd7ca9f59f35b430a259ceff10ab6af7b4ef6c
maserati12 <triggamaserati@gmail.com>
2020-02-08 16:00:01 +0300

    all other slides added

---
+++ slider.js
@@ -0,0 +1,5 @@
+// DOM Elements
+let allSlides = document.querySelectorAll('.slide');
+let prev = document.querySelector('.previous');
+let next = document.querySelector('.next');
+// Activating the first slide on load
